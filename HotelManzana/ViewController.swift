6efb7f92371dbc21f7c412f7a04c53c3b89b07e1
Jarvis Rojas <Jarvisro@icloud.com>
2019-09-28 10:32:16 -0400

    Initial Commit

---
+++ HotelManzana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelManzana
+//
+//  Created by Jarvis Rojas on 9/28/19.
+//  Copyright © 2019 Jarvis Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
